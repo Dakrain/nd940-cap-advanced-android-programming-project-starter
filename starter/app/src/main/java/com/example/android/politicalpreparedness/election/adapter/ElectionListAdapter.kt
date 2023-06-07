f9788a7b536b5f95cf0b491484e1c8ecf80e34f9
Khoa Le <lhdkhoa11th@gmail.com>
2023-06-07 16:06:20 +0700

    Add data binding for viewholder election

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -12,25 +12,31 @@ class ElectionListAdapter(private val clickListener: ElectionListener) :
     ListAdapter<Election, ElectionListAdapter.ElectionViewHolder>(ElectionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
-        return ElectionViewHolder.from(parent)
+        return ElectionViewHolder.from(parent, clickListener)
     }
 
     override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
-        holder.bind()
+        val item = getItem(position)
+        holder.bind(item)
     }
 
-    class ElectionViewHolder private constructor(val binding: ViewholderElectionBinding) :
+    class ElectionViewHolder private constructor(
+        val binding: ViewholderElectionBinding,
+        private val clickListener: ElectionListener
+    ) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind() {
-            //TODO Bind ViewHolder
+        fun bind(item: Election) {
+            binding.election = item
+            binding.clickListener = ElectionListener { election -> clickListener.onClick(election) }
+            binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent: ViewGroup): ElectionViewHolder {
+            fun from(parent: ViewGroup, clickListener: ElectionListener): ElectionViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ViewholderElectionBinding.inflate(layoutInflater, parent, false)
-                return ElectionViewHolder(binding)
+                return ElectionViewHolder(binding, clickListener)
             }
         }
     }
